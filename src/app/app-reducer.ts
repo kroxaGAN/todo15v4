6f6f970642a1163811e9d1f3d1b6a23f2df5ff69
Andrei Hutsau <mwplt@mail.ru>
2023-02-22 00:00:40 +0300

    made ErrorSnackbar

--- src/app/app-reducer.ts
+++ src/app/app-reducer.ts
@@ -1,25 +1,34 @@
+export type  StatusLoadingType = 'loading' | 'idle' | 'false' | 'successed'
 
-export type  StatusLoadingType='loading' | 'idle' | 'false' | 'successed'
-
-const initialAppState={
-    statusLoading:'idle' as StatusLoadingType
+const initialAppState = {
+    statusLoading: 'idle' as StatusLoadingType,
+    error: null as null | string
 }
 
-type InitialAppStateType=typeof initialAppState
+type InitialAppStateType = typeof initialAppState
 
-export const appReducer =(state:InitialAppStateType=initialAppState, action:AppActionType): InitialAppStateType=>{
+export const appReducer = (state: InitialAppStateType = initialAppState, action: AppActionType): InitialAppStateType => {
     switch (action.type) {
-        case 'CHANGE-LOADING-STATUS':{
-            return {...state,statusLoading: action.status}
+        case 'CHANGE-LOADING-STATUS': {
+            return {...state, statusLoading: action.status}
         }
-        default: return state
+        case 'CHANGE-ERROR-STATUS':{
+            return {...state, error: action.error}
+        }
+        default:
+            return state
     }
 }
 
-export const changeLoadingStatusAC=(status:StatusLoadingType)=>{
-    return{
-        type:"CHANGE-LOADING-STATUS",status
+export const changeLoadingStatusAC = (status: StatusLoadingType) => {
+    return {
+        type: "CHANGE-LOADING-STATUS", status
+    } as const
+}
+export const changeErrorStatusAC = (error: null | string) => {
+    return {
+        type: 'CHANGE-ERROR-STATUS', error
     }as const
 }
 
-export type AppActionType= ReturnType<typeof changeLoadingStatusAC>
+export type AppActionType = ReturnType<typeof changeLoadingStatusAC> | ReturnType<typeof changeErrorStatusAC>
